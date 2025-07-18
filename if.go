493939b9be09d59cb64feeb3707ac9e6ad324ef3
“registaalfe” <alfexcrstds07@gmail.com>
2025-07-18 15:06:08 +0700

    feat: added if.go

---
+++ if.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "John Doe"
+
+	if name == "John Doe" {
+		fmt.Println("Name is John Doe")
+	} else {
+		fmt.Println("Name is not John Doe")
+	}
+}
