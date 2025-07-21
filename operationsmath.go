9322648c5b3172243da7702819b85b8ebcd23556
“registaalfe” <alfexcrstds07@gmail.com>
2025-07-21 13:25:29 +0700

    feat: understanded a simple operations math

---
+++ operationsmath.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var a = 10 // create a new variable with value is 10
+	// a += 66 // added operations math with value 66
+
+	// fmt.Println("The value of a is:", a) // call the data so a = a + 66
+
+	var a = 10 // create a new variable with value is 10
+	a++ // added operations math with becomes a + 1
+
+	fmt.Println("The value of a is:", a) // call the data
+}
